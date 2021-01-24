c96c34fbd0b719d2149d29c3b0dca455d002b3d4
royweb <xytank@gmail.com>
2021-01-24 14:48:06 -0500

    Use useState() to store the user input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
       <h1>WRI Messenger</h1>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button>Send Message</button>
     </div>
   );
 }

c3d19c705098efea779a6bf300a84e4bceb144e6
royweb <xytank@gmail.com>
2021-01-24 15:16:44 -0500

    Add a function to send message

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = event => {
+    // `...` to get a copy of the array
+    setMessages([...messages, input]);
+
+    setInput('');
+  }
 
   return (
     <div className="App">
       <h1>WRI Messenger</h1>
       <input value={input} onChange={event => setInput(event.target.value)} />
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
+
+      {
+        messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))
+      }
     </div>
   );
 }

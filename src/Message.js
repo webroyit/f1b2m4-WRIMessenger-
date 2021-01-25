25eccc285ba4b6456c4e3fdc78921b68f47eb3cf
royweb <xytank@gmail.com>
2021-01-25 14:30:21 -0500

    Add material-ui styles on the message component

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 function Message(props) {
     return (
-        <div key={props.key}>
-            <p>{props.username}: {props.text}</p>
-        </div>
+        <Card key={props.key}>
+            <CardContent>
+                <Typography
+                    color="white"
+                    variant="h5"
+                    component="h2"
+                >
+                    {props.username}: {props.text}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
